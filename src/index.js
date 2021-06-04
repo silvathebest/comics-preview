dd2c3bd83c3074ba49d42a99f911536597d0540a
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-06-04 12:41:35 +0400

    proto is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import getComics from "./api/getComics.js";
 
 getComics(1235).then(item => console.log(item))
+
+const nextBtn = document.querySelector('#next')
+nextBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const index = window.location.href.indexOf('?') + 1
+    const parametr = Number(window.location.href.substring(index, window.location.href.length)) + 1
+    window.location.href = window.location.href.replace(parametr - 1, parametr)
+    getComics(parametr).then(item => {
+        document.querySelector('#picture').src = item.img
+    })
+})
+
+window.onload = () => {
+    const index = window.location.href.indexOf('?') + 1
+    let parametr = window.location.href.substring(index, window.location.href.length)
+    if (!index) parametr = 1234
+    getComics(parametr).then(item => {
+        document.querySelector('#picture').src = item.img
+    })
+}
