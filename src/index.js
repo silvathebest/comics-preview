ce08c3730e30b077522c169a6193f49fdd1059c1
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-06-04 19:14:15 +0400

    functional is ready

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
-import getComics from "./api/getComics.js";
+import getComics from "./api/getComics.js"
+import './styles/comics.css';
 
-getComics(1235).then(item => console.log(item))
+const defaultPictureId = 1234
+const maxPictureId = 2471
+const minPictureId = 1
+const nextBtn = document.querySelector('#nextBtn')
+const randomBtn = document.querySelector('#randomBtn')
+const prevBtn = document.querySelector('#prevBtn')
 
-const nextBtn = document.querySelector('#next')
-nextBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const index = window.location.href.indexOf('?') + 1
-    const parametr = Number(window.location.href.substring(index, window.location.href.length)) + 1
-    window.location.href = window.location.href.replace(parametr - 1, parametr)
-    getComics(parametr).then(item => {
-        document.querySelector('#picture').src = item.img
-    })
+prevBtn.addEventListener('click', () => navigationHadler(-1))
+
+randomBtn.addEventListener('click', () => {
+    const { index, pictureId, currentLocation } = createPictureId()
+    const randomPictureId = parseInt(Math.random() * (maxPictureId - minPictureId) + minPictureId)
+
+    index ? window.location.href = currentLocation.replace(pictureId, randomPictureId) :
+        window.location.href = `${currentLocation}?${randomPictureId}`
+
+    rerenederView()
 })
 
+nextBtn.addEventListener('click', () => navigationHadler(1))
+
+const navigationHadler = (button) => {
+    const { index, pictureId, currentLocation } = createPictureId()
+
+    index ? window.location.href = currentLocation.replace(pictureId, pictureId + button) :
+        window.location.href = `${currentLocation}?${defaultPictureId + button}`
+
+    rerenederView(pictureId)
+}
+
+const createPictureId = () => {
+    const currentLocation = window.location.href
+    const index = currentLocation.indexOf('?') + 1
+    const pictureId = Number(currentLocation.substring(index, currentLocation.length))
+
+    return { currentLocation, index, pictureId }
+}
+
 window.onload = () => {
-    const index = window.location.href.indexOf('?') + 1
-    let parametr = window.location.href.substring(index, window.location.href.length)
-    if (!index) parametr = 1234
-    getComics(parametr).then(item => {
-        document.querySelector('#picture').src = item.img
-    })
+    let { index, pictureId } = createPictureId()
+    if (!index) pictureId = 1234
+    rerenederView(pictureId)
+}
+
+const rerenederView = async(comicsId) => {
+    const comics = await getComics(comicsId)
+
+    console.log(comics)
+    document.querySelector('#title').innerHTML = comics.title
+    document.querySelector('#picture').src = comics.img
+    document.querySelector('#date').innerHTML = new Date(`${comics.month}.${comics.day}.${comics.year}`).toLocaleDateString()
+    document.querySelector('#transcript').innerHTML = comics.transcript
 }
